bb7799764ffb1cf81cfefc99bee30932c9b79d9e
M1-50-mariam <76564921+M1-50-mariam@users.noreply.github.com>
2020-12-25 21:24:34 +0300

    Update Ex1-water-bottles.swift

--- SwiftUICW2/SwiftUICW2/Ex1-water-bottles.swift
+++ SwiftUICW2/SwiftUICW2/Ex1-water-bottles.swift
@@ -7,6 +7,10 @@
 import SwiftUI
 
 struct Exercise1: View {
+    
+    @State var name = ""
+    @State var number = 0
+    
     var body: some View {
         ZStack {
             Image("charter")
@@ -21,9 +25,40 @@ struct Exercise1: View {
                 
 //                ADD THE CODE HERE
                 
+                TextField ("اكتب اسمك هنا \(name)", text: $name)
+                    
+                    .frame(width: 2.0)
                 
-                Spacer()        
-            }.padding()
+        
+                Stepper("كم بطلا من الماء تتعهد ان تشرب؟   .", value: $number, in: 1...100)
+                    .padding(.horizontal)
+                
+                
+                Spacer()
+                
+                
+            }
+            VStack(alignment: .center) {
+                Text("اتعهد انا")
+                    .font(.title)
+                
+                Text("\(name)")
+                    .font(.title)
+                Text("ان اشرب ")
+                    .font(.title)
+                Text("\(number)")
+                    
+                    .font(.title)
+                Text("من أكواب الماء يومياً")
+                    .font(.title)
+                Text("والله على ما أقول شهيد")
+                    .font(.title)
+                
+            }
+            
+            
+            
+            .padding()
         }
     }
 }
@@ -31,5 +66,6 @@ struct Exercise1: View {
 struct Exercise1_Previews: PreviewProvider {
     static var previews: some View {
         Exercise1()
+            .previewDevice("iPhone 11")
     }
 }
